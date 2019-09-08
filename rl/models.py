8846420522a6b3dc9ffb7d70add3e6099918b56b
princes <princesharzeel.10@gmail.com>
2019-09-08 10:01:10 +0530

    req

--- rl/models.py
+++ rl/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 class Vehicles(models.Model):
     reg_num=models.CharField(max_length=10,blank=False)
-    lat = models.CharField(max_length=8, blank=False)
-    lont = models.CharField(max_length=8, blank=False)
-    ign_status = models.CharField(max_length=8, blank=False)
-    fuel_lvl = models.CharField(max_length=8, blank=False)
-    tmstmp=models.CharField(max_length=20,blank=False)
+    lat = models.CharField(max_length=10, blank=False)
+    lont = models.CharField(max_length=10, blank=False)
+    ign_status = models.BooleanField(max_length=8, blank=False,default=False)
+    fuel_lvl = models.IntegerField( blank=False)
+    tmstmp=models.DateTimeField(max_length=20,blank=False)
 
     objects = models.Manager()
 
--- rl/views.py
+++ rl/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .forms import *
 from django.contrib import messages
 from .models import Vehicles
-import csv
+import csv,datetime,time
 
 def upload(request):
     if request.method == 'POST':
@@ -22,6 +22,8 @@ def upload(request):
                     db_obj.reg_num = str(fields[0])
                     db_obj.fuel_lvl = fields[3]
                     db_obj.ign_status = fields[2]
+                    ts = datetime.datetime.now().timestamp()
+                    print(ts)
                     db_obj.tmstmp=fields[4]
                     db_obj.lat=str(fields[1]).split("T")[0]
                     db_obj.lont=str(fields[1]).split("T")[1]
@@ -51,21 +53,35 @@ def home(request):
                 db_obj=Vehicles()
                 file_Reader =csv.reader(csv_file,delimiter=',')
                 row=1
-                for fields in file_Reader:
-                    if row==1:
-                        row=2
-                        continue
-                    db_obj.reg_num = str(fields[0])
-                    db_obj.fuel_lvl = fields[3]
-                    db_obj.ign_status = fields[2]
-                    db_obj.tmstmp=fields[4]
-                    db_obj.lat=str(fields[1]).split("T")[0]
-                    db_obj.lont=str(fields[1]).split("T")[1]
-                    db_obj.save()
 
-                #star = Loc.objects.filter(status='true')
-                messages.success(request, "Uploaded successfully")
-                return HttpResponseRedirect('/home')
+                try:
+                    for fields in file_Reader:
+                        if row==1:
+                            row=2
+                            continue
+                        db_obj.reg_num = str(fields[0])
+                        db_obj.fuel_lvl = fields[3]
+                        false_list=["FALSE","false","False","0"]
+                        true_list=["TRUE","true","True","1"]
+
+                        if fields[2] in false_list:
+                            db_obj.ign_status =False
+
+                        else:
+                            db_obj.ign_status =True
+
+                        db_obj.tmstmp=fields[4]
+                        db_obj.lat=str(fields[1]).split("T")[0]
+                        db_obj.lont=str(fields[1]).split("T")[1]
+                        db_obj.save()
+
+                    #star = Loc.objects.filter(status='true')
+                    messages.success(request, "Uploaded successfully")
+                    return HttpResponseRedirect('/home')
+                except:
+                    messages.error(request, "Invalid File Data")
+                    return HttpResponseRedirect('/home')
+
         else:
             messages.error(request, "Invalid File")
             return HttpResponse("/home")
@@ -73,5 +89,5 @@ def home(request):
     else:
         form = UploadCSV()
         db_obj=Vehicles.objects.all()
-        print(db_obj)
         return render(request, 'Home.html', {'form': form,'stp':db_obj})
+
